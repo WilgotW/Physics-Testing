d652872d2f11a75298bef0ee05179bca37017c18
Wilgot <wilgotwardell@hotmail.com>
2022-08-05 14:42:19 +0200

    mouse placement changes

--- script.js
+++ script.js
@@ -8,26 +8,31 @@ window.addEventListener('resize', function(){
     canvas.height = window.innerHeight;
     ground.y = canvas.height - canvas.height/10
 });
+const blockWidth = 50;
 
 class PhysicsObject{
-    constructor(x, y, color){
-        this.x = x ;
+    constructor(x, y, color, width, height){
+        this.x = x;
         this.y = y;
         this.color = color
-        this.width = 50;
-        this.height = 50;
+        this.width = width;
+        this.height = height;
         this.yVelocity = 0;
         this.xVelocity = 0;
         this.mass = 1;
         this.gravityScale = 1;
+        // this.rotation = h;
     }
     draw(){
+        // c.rotate(Math.PI / this.rotation);
         c.fillStyle = this.color;
         c.fillRect(this.x, this.y, this.width, this.height);
+        // c.setTransform(1, 0, 0, 1, 0, 0);
     }
     calcPhysics(){
         //gravity:
-        if(this.y + this.height > ground.y){
+        
+        if(this.y + this.height >= ground.y){
             this.gravityScale = 0;
             this.y = ground.y - this.height
         }
@@ -44,6 +49,7 @@ const ground = {
 
 let physObjects = [];
 let unvalidBlocks = [];
+
 const canvasBackground = 'white';
 const mouse = {
     x: 0,
@@ -56,9 +62,9 @@ window.addEventListener('mousemove', function(event){
 });
 window.addEventListener('mousedown', function(){
     for(i = 0; i < physObjects.length; i++){
-        if(mouse.x + physObjects[i].width > physObjects[i].x && mouse.x < physObjects[i].x + physObjects[i].width){
-            if(mouse.y + physObjects[i].height > physObjects[i].y && mouse.y < physObjects[i].y + physObjects[i].height){
-                unvalidBlocks.push(new PhysicsObject(mouse.x, mouse.y, 'rgba(255, 0, 0, 0.2)'));
+        if(mouse.x-blockWidth/2 + physObjects[i].width > physObjects[i].x && mouse.x-blockWidth/2 < physObjects[i].x + physObjects[i].width){
+            if(mouse.y-blockWidth/2 + physObjects[i].height > physObjects[i].y && mouse.y-blockWidth/2 < physObjects[i].y + physObjects[i].height){
+                unvalidBlocks.push(new PhysicsObject(mouse.x - blockWidth/2, mouse.y - blockWidth/2, 'rgba(255, 0, 0, 0.2)', 50, 50));
                 setTimeout(removeBlock, 100);
                 return;
             }      
@@ -67,11 +73,12 @@ window.addEventListener('mousedown', function(){
     if(mouse.y > ground.y){
         return;
     }
-    physObjects.push(new PhysicsObject(mouse.x, mouse.y, 'black'));
+    physObjects.push(new PhysicsObject(mouse.x - blockWidth/2, mouse.y - blockWidth/2, 'black', blockWidth, blockWidth));
 });
 
 function update(){
     refrech();
+    
     physObjects.forEach(physObject => {
         physObject.calcPhysics();
         physObject.draw();
@@ -87,7 +94,7 @@ function update(){
     
     requestAnimationFrame(update);
 }
-update();
+update()
 
 function removeBlock(){
     unvalidBlocks.splice(0, 1);
@@ -97,9 +104,9 @@ function checkCollisions(arr){
     for(let i = 0; i < arr.length; i++){
         for(let z = i+1; z < arr.length; z++){
             //x touching:
-            if(arr[i].x + arr[i].width > arr[z].x && arr[i].x < arr[z].x + arr[z].width){
+            if(arr[i].x + arr[i].width >= arr[z].x && arr[i].x <= arr[z].x + arr[z].width){
                 //y touching:
-                if(arr[i].y + arr[i].height > arr[z].y && arr[i].y < arr[z].y + arr[z].height){
+                if(arr[i].y + arr[i].height >= arr[z].y && arr[i].y <= arr[z].y + arr[z].height){
                     arr[z].gravityScale = 0;
                     arr[z].y = arr[i].y - arr[z].height;
                 }
